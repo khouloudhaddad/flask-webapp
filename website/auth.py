d95274f8cc988813c196aa822de9c3283d8fcb28
khouloud HADDAD AMAMOU <khouloud.haddad@gmail.com>
2024-01-11 00:23:40 +0100

    Sign up page

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,9 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from . import db
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
 
 auth = Blueprint('auth', __name__)
 
@@ -28,6 +33,11 @@ def sign_up():
             flash('Password must be at least 7 characters.', category='error')
         else:
             # Add user to database
+            new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('User account successfully created.', category='success')
+            # Redirect user to home page
+            return redirect(url_for('views.home'))
 
     return render_template("sign_up.html")
