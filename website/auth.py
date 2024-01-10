e1478e3011e656cee381789f8c7eb10cc4485a86
khouloud HADDAD AMAMOU <khouloud.haddad@gmail.com>
2024-01-10 20:52:32 +0100

    Templating

--- website/auth.py
+++ website/auth.py
@@ -1,7 +1,16 @@
 from flask import Blueprint
+from flask import render_template
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/')
+@auth.route('/login')
 def login():
-    return "<h1>Login</h1>"
+    return render_template("login.html", text='Testing')
+
+@auth.route('/logout')
+def logout():
+    return "<h1>Logout</h1>"
+
+@auth.route('/sign-up')
+def sign_up():
+    return "<h1>Sign up</h1>"
