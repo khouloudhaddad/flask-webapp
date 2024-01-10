46d49dad857c1f5bebc30705df8dfb7faa6a57c3
khouloud HADDAD AMAMOU <khouloud.haddad@gmail.com>
2024-01-11 00:48:56 +0100

    Adjust menu links

--- website/auth.py
+++ website/auth.py
@@ -24,7 +24,7 @@ def login():
         else:
             flash('Email does not exist ! ', category="error")
 
-    return render_template("login.html")
+    return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
 @login_required
@@ -62,4 +62,4 @@ def sign_up():
             # Redirect user to home page
             return redirect(url_for('views.home'))
 
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
